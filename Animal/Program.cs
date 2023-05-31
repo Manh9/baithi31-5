0fb684dc4030e974a8b73aff082a4937b9a3d4b8
Manh9 <112113077+Manh9@users.noreply.github.com>
2023-05-31 10:13:59 +0700

    Add files via upload

---
+++ Animal/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lion lion = new Lion(200, "lion");
+            Tiger tiger = new Tiger(100, "tiger");
+
+            lion.Show();
+            tiger.Show();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GeometryExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the dimensions of the cylinder:");
+            Console.Write("Radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Cylinder cylinder = new Cylinder(radius, height);
+            cylinder.Process();
+
+            Console.ReadLine();
+        }
+    }
+}
